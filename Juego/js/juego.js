539dbebc1d1d4941fdcfbbebfca8315d6d0efa38
Salvador Jimenez <salvad.j.f@gmail.com>
2023-11-22 21:16:31 +0100

    22 de Noviembre, Entorno Cliente

--- Juego/js/juego.js
+++ Juego/js/juego.js
@@ -13,18 +13,17 @@ class Jugador {
 
         this.rotacion = 0;
 
-        const image = new Image();
-        image.src = "./img/nave.png";
-        image.onload = () => {
+        this.image = new Image();
+        this.image.src = "./img/nave.png";
+        this.image.onload = () => {
             const escala = 0.15;
-            this.image = image;
-            this.width = image.width * escala;
-            this.height = image.height * escala;
+            this.width = this.image.width * escala;
+            this.height = this.image.height * escala;
             this.posicion = {
                 x: canvas.width / 2 - this.width / 2,
                 y: canvas.height - this.height - 20,
             };
-        };
+        }
     }
 
     crear() {
@@ -55,6 +54,45 @@ class Jugador {
     }
 }
 
+class Alien {
+    constructor({posicion}) {
+        this.velocidad = {
+            x: 2, // Establecer una velocidad inicial
+            y: 0,
+        };
+
+        this.image = new Image();
+        this.image.src = "./img/alien.png";
+        this.image.onload = () => {
+            const escala = 0.05;
+            this.width = this.image.width * escala;
+            this.height = this.image.height * escala;
+            this.posicion = {
+                x: posicion.x,
+                y: posicion.y,
+            };
+        };
+    }
+
+    crear() {
+        c.drawImage(
+            this.image,
+            this.posicion.x,
+            this.posicion.y,
+            this.width,
+            this.height
+        );
+    }
+
+    actualizar() {
+        if (this.image) {
+            this.crear();
+            this.posicion.x += this.velocidad.x;
+            this.posicion.y += this.velocidad.y;
+        }
+    }
+}
+
 class Proyectil {
     constructor({ posicion, velocidad }) {
         this.posicion = posicion;
@@ -66,7 +104,7 @@ class Proyectil {
         c.beginPath();
         c.arc(this.posicion.x, this.posicion.y, this.radio, 0, Math.PI * 2);
 
-        c.fillStyle = 'red';
+        c.fillStyle = "red";
         c.fill();
         c.closePath();
     }
@@ -78,8 +116,60 @@ class Proyectil {
     }
 }
 
+class Cuadricula {
+    constructor() {
+        this.posicion = {
+            x: 0,
+            y: 0,
+        };
+        this.velocidad = {
+            x: 2, // Establecer una velocidad inicial
+            y: 0,
+        };
+
+        this.aliens = [];
+
+        const lineas = Math.floor(Math.random() * 10 + 5);
+        const columnas = Math.floor(Math.random() * 5 + 2);
+
+        this.width = columnas * 30;
+
+        for (let i = 0; i < columnas; i++) {
+            for (let j = 0; j < lineas; j++) {
+                this.aliens.push(
+                    new Alien({
+                        posicion: {
+                            x: i * 30,
+                            y: j * 30,
+                        },
+                    })
+                );
+            }
+        }
+        console.log(this.aliens);
+    }
+
+    actualizar() {
+        this.posicion.x += this.velocidad.x;
+
+        if (
+            this.posicion.x + this.width >= canvas.width ||
+            this.posicion.x <= 0
+        ) {
+            this.velocidad.x = -this.velocidad.x;
+            this.velocidad.y = 30;
+        }
+
+        this.aliens.forEach((alien) => {
+            alien.actualizar();
+        });
+    }
+}
+
 const jugador = new Jugador();
+const cuadricula = new Cuadricula();
 const proyectiles = [];
+
 const teclas = {
     a: {
         presionada: false,
@@ -97,6 +187,11 @@ function animar() {
     c.fillStyle = "black";
     c.fillRect(0, 0, canvas.width, canvas.height);
     jugador.actualizar();
+    cuadricula.actualizar();
+    cuadricula.aliens.forEach((alien) => {
+        alien.actualizar();
+    });
+
     proyectiles.forEach((proyectil, indice) => {
         if (proyectil.posicion.y + proyectil.radio <= 0) {
             setTimeout(() => {
@@ -134,16 +229,18 @@ addEventListener("keydown", ({ key }) => {
             break;
         case "ArrowUp":
             teclas.ArrowUp.presionada = true;
-            proyectiles.push(new Proyectil({
-                posicion: {
-                    x: jugador.posicion.x + jugador.width / 2,
-                    y: jugador.posicion.y,
-                },
-                velocidad: {
-                    x: 0,
-                    y: -10,
-                },
-            }));
+            proyectiles.push(
+                new Proyectil({
+                    posicion: {
+                        x: jugador.posicion.x + jugador.width / 2,
+                        y: jugador.posicion.y,
+                    },
+                    velocidad: {
+                        x: 0,
+                        y: -10,
+                    },
+                })
+            );
             break;
         default:
             break;
@@ -160,16 +257,18 @@ addEventListener("keyup", ({ key }) => {
             break;
         case "ArrowUp":
             teclas.ArrowUp.presionada = false;
-            proyectiles.push(new Proyectil({
-                posicion: {
-                    x: jugador.posicion.x + jugador.width / 2,
-                    y: jugador.posicion.y,
-                },
-                velocidad: {
-                    x: 0,
-                    y: -10,
-                },
-            }));
+            proyectiles.push(
+                new Proyectil({
+                    posicion: {
+                        x: jugador.posicion.x + jugador.width / 2,
+                        y: jugador.posicion.y,
+                    },
+                    velocidad: {
+                        x: 0,
+                        y: -10,
+                    },
+                })
+            );
             break;
         default:
             break;
