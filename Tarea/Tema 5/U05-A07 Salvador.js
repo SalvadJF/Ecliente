280ce0eb0b4a179ce54c20ef1cf2d49fb4b477ec
Salvador Jimenez <salvad.j.f@gmail.com>
2024-01-10 21:10:26 +0100

    10 de Enero, Entorno Cliente

--- Tarea/Tema 5/U05-A07 Salvador.js
+++ Tarea/Tema 5/U05-A07 Salvador.js
@@ -1,14 +1,19 @@
-let mensaje = document.getElementById("mensaje");
-let imagen = document.getElementsByTagName('img');
 
-function mensajeError() {
-  mensaje.innerHTML = "<h1>La imagen no se ha podido cargar</h1>";
+var mensaje = document.getElementById("mensaje");
+var imagen = document.getElementsByTagName("img")[0];
+
+function error() {
+  mensaje.innerHTML = "<h1>La imagen no se ha cargado correctamente</h1>"
 }
 
-function mensajeConfirmacion() {
-  mensaje.innerHTML = "<h1>La imagen se ha cargado correctamente</h1>";
+function carga() {
+  mensaje.innerHTML = "<h1>La imagen se ha cargado correctamente</h1>"
 }
 
-imagen.addEventListener('error', mensajeError);
-imagen.addEventListener('abort', mensajeError);
-imagen.addEventListener('load', mensajeConfirmacion);
+imagen.onload = function() {
+  carga();
+};
+
+imagen.onerror = function() {
+  error();
+};
--- Tarea/Tema 5/U05-A08 Salvador.js
+++ Tarea/Tema 5/U05-A08 Salvador.js
@@ -2,17 +2,6 @@ function enviarGoogle() {
   window.location.href = 'https://www.google.com';
 }
 
-function limpiarFormulario() {
-  document.getElementById('nombre').value = '';
-  document.getElementById('mensaje').value = '';
-  document.getElementById('rojo').checked = false;
-  document.getElementById('verde').checked = false;
-  document.getElementById('azul').checked = false;
-  document.getElementById('lunes').checked = false;
-  document.getElementById('martes').checked = false;
-  document.getElementById('miercoles').checked = false;
-  document.getElementById('jueves').checked = false;
-  document.getElementById('viernes').checked = false;
-  document.getElementsByName('asignatura')[0].selectedIndex = 0;
-  document.getElementsByName('preferencia')[0].selectedIndex = 0;
-}
+const botonEnviar = document.querySelector(".enviar");
+let asignatura = document.getElementById("asignatura");
+
