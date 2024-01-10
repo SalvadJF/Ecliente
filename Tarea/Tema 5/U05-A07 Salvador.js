e4a6aaf2a6d59432ff09929b74746d3c3f923d34
Salvador Jimenez <salvad.j.f@gmail.com>
2024-01-10 14:19:59 +0100

    cambios

--- Tarea/Tema 5/U05-A07 Salvador.js
+++ Tarea/Tema 5/U05-A07 Salvador.js
@@ -3,20 +3,12 @@ let imagen = document.getElementsByTagName('img');
 
 function mensajeError() {
   mensaje.innerHTML = "<h1>La imagen no se ha podido cargar</h1>";
-
 }
 
 function mensajeConfirmacion() {
   mensaje.innerHTML = "<h1>La imagen se ha cargado correctamente</h1>";
-
 }
 
-if (imagen.addEventListener('error')) {
-  mensajeError()
-}
-if (imagen.addEventListener('abort')) {
-  mensajeError()
-}
-if (imagen.addEventListener('load')) {
-  mensajeConfirmacion()
-}
+imagen.addEventListener('error', mensajeError);
+imagen.addEventListener('abort', mensajeError);
+imagen.addEventListener('load', mensajeConfirmacion);
--- Tarea/Tema 5/U05-A08 Salvador.js
+++ Tarea/Tema 5/U05-A08 Salvador.js
@@ -1,3 +1,18 @@
 function enviarGoogle() {
-  window.location.replace("https://www.google.com/")
+  window.location.href = 'https://www.google.com';
+}
+
+function limpiarFormulario() {
+  document.getElementById('nombre').value = '';
+  document.getElementById('mensaje').value = '';
+  document.getElementById('rojo').checked = false;
+  document.getElementById('verde').checked = false;
+  document.getElementById('azul').checked = false;
+  document.getElementById('lunes').checked = false;
+  document.getElementById('martes').checked = false;
+  document.getElementById('miercoles').checked = false;
+  document.getElementById('jueves').checked = false;
+  document.getElementById('viernes').checked = false;
+  document.getElementsByName('asignatura')[0].selectedIndex = 0;
+  document.getElementsByName('preferencia')[0].selectedIndex = 0;
 }
