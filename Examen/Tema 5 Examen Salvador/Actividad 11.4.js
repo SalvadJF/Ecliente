cc114923856457a7eaf4a6ddef92220a88ff8478
Salvador Jimenez <salvad.j.f@gmail.com>
2024-01-31 17:27:20 +0100

    cambio

--- Examen/Tema 5 Examen Salvador/Actividad 11.4.js
+++ Examen/Tema 5 Examen Salvador/Actividad 11.4.js
@@ -14,6 +14,31 @@ function getCookie(name) {
   return null;
 }
 
+function setCookie(name, value, options = {}) {
+  options = {
+    path: "/",
+    // agregar otros valores predeterminados si es necesario
+    ...options,
+  };
+
+  if (options.expires instanceof Date) {
+    options.expires = options.expires.toUTCString();
+  }
+
+  let updatedCookie =
+    encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+  for (let optionKey in options) {
+    updatedCookie += "; " + optionKey;
+    let optionValue = options[optionKey];
+    if (optionValue !== true) {
+      updatedCookie += "=" + optionValue;
+    }
+  }
+
+  document.cookie = updatedCookie;
+}
+
 function deleteCookie(name) {
   setCookie(name, "", {
     "max-age": -1,
