e0e912582683b585fecd3b1aa700cee9f9af5b9d
Salvador Jimenez <salvad.j.f@gmail.com>
2024-02-02 19:20:59 +0100

    cambios

--- Tarea/Tema 6/U06-A07 Salvador.js
+++ Tarea/Tema 6/U06-A07 Salvador.js
@@ -1,46 +1,17 @@
-// Función para mostrar el DOM en el contenedor
-function mostrarDOM() {
-    // Obtener el elemento raíz del DOM (document)
-    var rootNode = document;
+function mostrarDOM(elemento, lista = document.createElement('ul')) {
+    let objeto = document.createElement('li');
+    objeto.textContent = elemento.tagName;
+    lista.appendChild(objeto);
 
-    // Llamar a la función recursiva para construir la representación del árbol DOM
-    var domTree = buildDOMTree(rootNode);
+    Array.from(elemento.children).forEach((hijo) => {
+        mostrarDOM(hijo, objeto.appendChild(document.createElement('ul')));
+    });
 
-    // Mostrar el resultado en el contenedor
-    var domViewer = document.getElementById("domViewer");
-    domViewer.innerHTML = domTree;
+    return lista;
 }
 
-// Función recursiva para construir la representación del árbol DOM
-function buildDOMTree(node) {
-    var result = '<ul>';
-    result += '<li>' + getNodeString(node) + '</li>';
-    
-    // Recorrer los nodos hijos
-    for (var i = 0; i < node.children.length; i++) {
-        result += buildDOMTree(node.children[i]);
-    }
-
-    result += '</ul>';
-    return result;
+window.onload = function() {
+    let html = document.documentElement;
+    let resultado = document.getElementById('resultado');
+    resultado.appendChild(mostrarDOM(html));
 }
-
-// Función para obtener la cadena que representa un nodo del DOM
-function getNodeString(node) {
-    var nodeString = node.nodeName;
-
-    // Agregar el ID si existe
-    if (node.id) {
-        nodeString += ' (id: ' + node.id + ')';
-    }
-
-    // Agregar las clases si existen
-    if (node.classList.length > 0) {
-        nodeString += ' (class: ' + Array.from(node.classList).join(', ') + ')';
-    }
-
-    return nodeString;
-}
-
-// Llamar a la función al cargar la página
-window.onload = mostrarDOM;
