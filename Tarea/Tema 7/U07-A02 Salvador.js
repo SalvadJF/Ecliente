9d389393678aca4d4f4f994cb11b284af05b5901
Salvador Jimenez <salvad.j.f@gmail.com>
2024-02-06 21:09:09 +0100

    6 de Febrero, Entorno Cliente

--- Tarea/Tema 7/U07-A02 Salvador.js
+++ Tarea/Tema 7/U07-A02 Salvador.js
@@ -1,39 +1,16 @@
-var ciudad = document.getElementById('ciudad');
-var sugerencias = document.getElementById('sugerencias');
+const sugerenciasCiudades =  document.getElementById('sugerencias');
+async function sugerencia() {
 
-ciudad.addEventListener('keyup', () => {
-    let ciudadNombre = ciudad.value.trim();
-    if (ciudadNombre.length >= 2) {
-        let xhr = new XMLHttpRequest();
-        xhr.open('GET', `./ciudadessugerencias.php?city=${ciudadNombre}`);
-        xhr.onload = () => {
-            if (xhr.status === 200) {
-                let ciudadLista = JSON.parse(xhr.responseText);
-                displaySugerencias(ciudadLista);
-            }
-        };
-        xhr.send();
-    } else {
-        sugerencias.innerHTML = '';
-        sugerencias.style.display = 'none';
-    }
-});
+    const peticionURL = './Ciudades.json';
+    const peticion = new Request(peticionURL);
 
-function displaySugerencias(ciudadLista) {
-    sugerencias.innerHTML = '';
-    if (ciudadLista.length > 0) {
-        sugerencias.style.display = 'block';
-        ciudadLista.forEach(city => {
-            let div = document.createElement('div');
-            div.textContent = city;
-            div.addEventListener('click', () => {
-                ciudad.value = city;
-                sugerencias.innerHTML = '';
-                sugerencias.style.display = 'none';
-            });
-            sugerencias.appendChild(div);
-        });
-    } else {
-        sugerencias.style.display = 'none';
-    }
+    const respuesta = await fetch(peticion);
+    const sugerenciasTexto = await respuesta.text();
+
+    const sugerencias = JSON.parse(sugerenciasTexto);
+
+    sugerenciasCiudades.innerHTML = sugerencias
+
 }
+
+window.onchange(sugerencia())
