6290b9b52fdc9e9d2f98d4f95598b06a3c26b8aa
Salvador Jimenez <salvad.j.f@gmail.com>
2024-02-21 12:35:54 +0100

    Cambios

--- Proyecto/proyectoSalvador/src/components/Header.jsx
+++ Proyecto/proyectoSalvador/src/components/Header.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header>
-      {/* Tu contenido del encabezado */}
+<header className="fixed w-full h-16 bg-black bg-opacity-70">
+      <nav className="max-w-4xl mx-auto">
+        <a href="index.html" className="font-geostar text-5xl font-bold text-red-600 no-underline inline-block pt-4">TTT</a>
+        <ul className="float-right flex items-center">
+          <li><a href="index.html" className="text-white no-underline px-4 py-3 bg-red-700 rounded-l-xl">Inicio</a></li>
+          <li><a href="articulo.html" className="text-white no-underline px-4 py-3 bg-red-700">Artículo</a></li>
+          <li><a href="contacto.html" className="text-white no-underline px-4 py-3 bg-red-700 rounded-r-xl">Contacto</a></li>
+        </ul>
+      </nav>
     </header>
   );
 }
